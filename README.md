50f676ac7dbdccf6041847721d621db9b5478fd8
Dogeblox <101982761+Dogeblox@users.noreply.github.com>
2022-03-20 17:56:23 +0300

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# DOGEBLOX Smart Contract
+
+## Overview
+
+This package includes ERC-20 token smart contract.
+
+- Token Name: DOGEBLOX
+- Token Symbol: $DOBX
+- Total Supply: 1,000,000,000
+- Decimals: 9
+
+## How to Deploy
+
+Use Remix IDE or deployment tools like Hardhat, Truffle to deploy this contract.
+
+https://remix.ethereum.org
+https://hardhat.org/guides/deploying.html
+https://trufflesuite.com/docs/truffle/quickstart.html
---
+++ contracts/DOGEBLOX.sol
@@ -0,0 +1,359 @@
+// SPDX-License-Identifier: MIT
+
+/**
+    ██████╗  ██████╗  ██████╗ ███████╗██████╗ ██╗      ██████╗ ██╗  ██╗
+    ██╔══██╗██╔═══██╗██╔════╝ ██╔════╝██╔══██╗██║     ██╔═══██╗╚██╗██╔╝
+    ██║  ██║██║   ██║██║  ███╗█████╗  ██████╔╝██║     ██║   ██║ ╚███╔╝ 
+    ██║  ██║██║   ██║██║   ██║██╔══╝  ██╔══██╗██║     ██║   ██║ ██╔██╗ 
+    ██████╔╝╚██████╔╝╚██████╔╝███████╗██████╔╝███████╗╚██████╔╝██╔╝ ██╗
+    ╚═════╝  ╚═════╝  ╚═════╝ ╚══════╝╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+ */
+
+pragma solidity 0.8.13;
+
+import {Context} from "./Context.sol";
+import {IERC20} from "./IERC20.sol";
+import {Ownable} from "./Ownable.sol";
+import {SafeMath} from "./SafeMath.sol";
+import {Address} from "./Address.sol";
+import {IUniswapV2Factory} from "./IUniswapV2Factory.sol";
+import {IUniswapV2Pair} from "./IUniswapV2Pair.sol";
+import {IUniswapV2Router01} from "./IUniswapV2Router01.sol";
+import {IUniswapV2Router02} from "./IUniswapV2Router02.sol";
+
+contract DOGEBLOX is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+    using Address for address;
+    
+    string private constant _name = "DOGEBLOX";
+    string private constant _symbol = "DOBX";
+    uint256 private constant _totalSupply = 1 * 10**9 * 10**_decimals; // 1,000,000,000
+    uint8 private constant _decimals = 9;
+
+    address payable public marketingWalletAddress = payable(0x531e5503b4c1621FDcAB5fE9eD9E2670a8F8908A);
+    address payable public teamWalletAddress = payable(0xB3c30067E1e02784356169680E3D0d3b1cFC7Ba1);
+    address public immutable deadAddress = 0x000000000000000000000000000000000000dEaD;
+    
+    mapping (address => uint256) _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+    
+    mapping (address => bool) public isExcludedFromFee;
+    mapping (address => bool) public isMarketPair;
+
+    uint256 public _buyLiquidityFee = 2;
+    uint256 public _buyMarketingFee = 2;
+    uint256 public _buyTeamFee = 1;
+    
+    uint256 public _sellLiquidityFee = 2;
+    uint256 public _sellMarketingFee = 2;
+    uint256 public _sellTeamFee = 1;
+
+    uint256 public _liquidityShare = 4;
+    uint256 public _marketingShare = 4;
+    uint256 public _teamShare = 16;
+
+    uint256 public _totalTaxIfBuying = 5;
+    uint256 public _totalTaxIfSelling = 5;
+    uint256 public _totalDistributionShares = 24;
+
+    IUniswapV2Router02 public uniswapV2Router;
+    address public uniswapPair;
+    
+    bool inSwapAndLiquify;
+    bool public swapAndLiquifyEnabled = true;
+    bool public swapAndLiquifyByLimitOnly = false;
+
+    event SwapAndLiquifyEnabledUpdated(bool enabled);
+    event SwapAndLiquify(
+        uint256 tokensSwapped,
+        uint256 ethReceived,
+        uint256 tokensIntoLiqudity
+    );
+    
+    event SwapETHForTokens(
+        uint256 amountIn,
+        address[] path
+    );
+    
+    event SwapTokensForETH(
+        uint256 amountIn,
+        address[] path
+    );
+    
+    modifier lockTheSwap {
+        inSwapAndLiquify = true;
+        _;
+        inSwapAndLiquify = false;
+    }
+    
+    constructor () {
+        /**
+         * @dev Routers config for PancakeSwap
+         *
+         * PancakeSwap v2 Mainnet Router Address: 0x10ED43C718714eb63d5aA57B78B54704E256024E
+         * PancakeSwap v2 Testnet Router Address: 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
+         */
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
+
+        uniswapPair = IUniswapV2Factory(_uniswapV2Router.factory())
+            .createPair(address(this), _uniswapV2Router.WETH());
+
+        uniswapV2Router = _uniswapV2Router;
+        _allowances[address(this)][address(uniswapV2Router)] = _totalSupply;
+
+        isExcludedFromFee[owner()] = true;
+        isExcludedFromFee[address(this)] = true;
+        
+        _totalTaxIfBuying = _buyLiquidityFee.add(_buyMarketingFee).add(_buyTeamFee);
+        _totalTaxIfSelling = _sellLiquidityFee.add(_sellMarketingFee).add(_sellTeamFee);
+        _totalDistributionShares = _liquidityShare.add(_marketingShare).add(_teamShare);
+
+        isMarketPair[address(uniswapPair)] = true;
+
+        _balances[_msgSender()] = _totalSupply;
+        emit Transfer(address(0), _msgSender(), _totalSupply);
+    }
+
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view virtual returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "DOGEBLOX: decreased allowance below zero"));
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) private {
+        require(owner != address(0), "DOGEBLOX: approve from the zero address");
+        require(spender != address(0), "DOGEBLOX: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function airdrop(address[] calldata recipients, uint256[] calldata amounts) public onlyOwner returns (bool) {
+        require(recipients.length == amounts.length, "DOGEBLOX: recipients and amounts must be the same length");
+        for (uint i = 0; i < recipients.length; i++) {
+            _transfer(_msgSender(), recipients[i], amounts[i]);
+        }
+        return true;
+    }
+
+    function setMarketPairStatus(address account, bool newValue) public onlyOwner {
+        isMarketPair[account] = newValue;
+    }
+    
+    function setIsExcludedFromFee(address account, bool newValue) public onlyOwner {
+        isExcludedFromFee[account] = newValue;
+    }
+
+    function setBuyTaxes(uint256 newLiquidityTax, uint256 newMarketingTax, uint256 newTeamTax) external onlyOwner() {
+        _buyLiquidityFee = newLiquidityTax;
+        _buyMarketingFee = newMarketingTax;
+        _buyTeamFee = newTeamTax;
+
+        _totalTaxIfBuying = _buyLiquidityFee.add(_buyMarketingFee).add(_buyTeamFee);
+    }
+
+    function setSelTaxes(uint256 newLiquidityTax, uint256 newMarketingTax, uint256 newTeamTax) external onlyOwner() {
+        _sellLiquidityFee = newLiquidityTax;
+        _sellMarketingFee = newMarketingTax;
+        _sellTeamFee = newTeamTax;
+
+        _totalTaxIfSelling = _sellLiquidityFee.add(_sellMarketingFee).add(_sellTeamFee);
+    }
+    
+    function setDistributionSettings(uint256 newLiquidityShare, uint256 newMarketingShare, uint256 newTeamShare) external onlyOwner() {
+        _liquidityShare = newLiquidityShare;
+        _marketingShare = newMarketingShare;
+        _teamShare = newTeamShare;
+
+        _totalDistributionShares = _liquidityShare.add(_marketingShare).add(_teamShare);
+    }
+
+    function setMarketingWalletAddress(address newAddress) external onlyOwner() {
+        marketingWalletAddress = payable(newAddress);
+    }
+
+    function setTeamWalletAddress(address newAddress) external onlyOwner() {
+        teamWalletAddress = payable(newAddress);
+    }
+
+    function setSwapAndLiquifyEnabled(bool _enabled) public onlyOwner {
+        swapAndLiquifyEnabled = _enabled;
+        emit SwapAndLiquifyEnabledUpdated(_enabled);
+    }
+
+    function setSwapAndLiquifyByLimitOnly(bool newValue) public onlyOwner {
+        swapAndLiquifyByLimitOnly = newValue;
+    }
+    
+    function getCirculatingSupply() public view returns (uint256) {
+        return _totalSupply.sub(balanceOf(deadAddress));
+    }
+
+    function transferToAddressETH(address payable recipient, uint256 amount) private {
+        recipient.transfer(amount);
+    }
+    
+    function changeRouterVersion(address newRouterAddress) public onlyOwner returns(address newPairAddress) {
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(newRouterAddress); 
+
+        newPairAddress = IUniswapV2Factory(_uniswapV2Router.factory()).getPair(address(this), _uniswapV2Router.WETH());
+
+        if (newPairAddress == address(0)) { //Create If Doesnt exist
+            newPairAddress = IUniswapV2Factory(_uniswapV2Router.factory())
+                .createPair(address(this), _uniswapV2Router.WETH());
+        }
+
+        uniswapPair = newPairAddress; //Set new pair address
+        uniswapV2Router = _uniswapV2Router; //Set new router address
+
+        isMarketPair[address(uniswapPair)] = true;
+    }
+
+    // to recieve ETH from uniswapV2Router when swaping
+    receive() external payable {}
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "DOGEBLOX: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) private returns (bool) {
+        require(sender != address(0), "DOGEBLOX: transfer from the zero address");
+        require(recipient != address(0), "DOGEBLOX: transfer to the zero address");
+
+        if (inSwapAndLiquify) { 
+            return _basicTransfer(sender, recipient, amount); 
+        } else {
+            _balances[sender] = _balances[sender].sub(amount, "DOGEBLOX: Insufficient Balance");
+
+            uint256 finalAmount = (isExcludedFromFee[sender] || isExcludedFromFee[recipient]) ? 
+                                        amount : takeFee(sender, recipient, amount);
+
+            _balances[recipient] = _balances[recipient].add(finalAmount);
+
+            emit Transfer(sender, recipient, finalAmount);
+            return true;
+        }
+    }
+
+    function _basicTransfer(address sender, address recipient, uint256 amount) internal returns (bool) {
+        _balances[sender] = _balances[sender].sub(amount, "DOGEBLOX: Insufficient Balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function swapAndLiquify(uint256 tAmount) private lockTheSwap {
+        uint256 tokensForLP = tAmount.mul(_liquidityShare).div(_totalDistributionShares).div(2);
+        uint256 tokensForSwap = tAmount.sub(tokensForLP);
+
+        swapTokensForEth(tokensForSwap);
+        uint256 amountReceived = address(this).balance;
+
+        uint256 totalBNBFee = _totalDistributionShares.sub(_liquidityShare.div(2));
+        
+        uint256 amountBNBLiquidity = amountReceived.mul(_liquidityShare).div(totalBNBFee).div(2);
+        uint256 amountBNBTeam = amountReceived.mul(_teamShare).div(totalBNBFee);
+        uint256 amountBNBMarketing = amountReceived.sub(amountBNBLiquidity).sub(amountBNBTeam);
+
+        if (amountBNBMarketing > 0)
+            transferToAddressETH(marketingWalletAddress, amountBNBMarketing);
+
+        if (amountBNBTeam > 0)
+            transferToAddressETH(teamWalletAddress, amountBNBTeam);
+
+        if (amountBNBLiquidity > 0 && tokensForLP > 0)
+            addLiquidity(tokensForLP, amountBNBLiquidity);
+    }
+    
+    function swapTokensForEth(uint256 tokenAmount) private {
+        // generate the uniswap pair path of token -> weth
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // make the swap
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            tokenAmount,
+            0, // accept any amount of ETH
+            path,
+            address(this), // The contract
+            block.timestamp
+        );
+        
+        emit SwapTokensForETH(tokenAmount, path);
+    }
+
+    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
+        // approve token transfer to cover all possible scenarios
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // add the liquidity
+        uniswapV2Router.addLiquidityETH{value: ethAmount}(
+            address(this),
+            tokenAmount,
+            0, // slippage is unavoidable
+            0, // slippage is unavoidable
+            owner(),
+            block.timestamp
+        );
+    }
+
+    function takeFee(address sender, address recipient, uint256 amount) internal returns (uint256) {
+        uint256 feeAmount = 0;
+        
+        if (isMarketPair[sender]) {
+            feeAmount = amount.mul(_totalTaxIfBuying).div(100);
+        } else if (isMarketPair[recipient]) {
+            feeAmount = amount.mul(_totalTaxIfSelling).div(100);
+        }
+        
+        if (feeAmount > 0) {
+            _balances[address(this)] = _balances[address(this)].add(feeAmount);
+            emit Transfer(sender, address(this), feeAmount);
+        }
+
+        return amount.sub(feeAmount);
+    }
+}
